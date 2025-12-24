2570deb8d4b60f6f51732fa6636c5a6a6e21bb4c
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-12-24 21:13:56 +0500

    Update react-entry.jsx

--- src/react-entry.jsx
+++ src/react-entry.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import FullscreenTool from './FullscreenTool.jsx'; 
+import FullscreenTool from './CustomScreen.jsx'; 
 
 
 // Find the container element
